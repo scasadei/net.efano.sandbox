c561db7ca8a56175b08dca12bd58b6a43172e876
stefano casadei <scasadei@yahoo.com>
2011-06-17 15:24:21 +0200

    first commit, will it go in the right place?

---
+++ net.efano.sandbox.jface.tableviewer/src/net/efano/sandbox/jface/tableviewer/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package net.efano.sandbox.jface.tableviewer;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "net.efano.sandbox.jface.tableviewer.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
---
+++ net.efano.sandbox.jface.treeviewer/src/net/efano/sandbox/jface/treeviewer/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package net.efano.sandbox.jface.treeviewer;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "net.efano.sandbox.jface.treeviewer.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
